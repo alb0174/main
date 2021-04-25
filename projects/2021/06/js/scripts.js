a44329af89939a4fc8d68ebab67ad7587a4aea5a
alb0174 <41185492+alb0174@users.noreply.github.com>
2021-04-25 13:52:53 +0200

    Update scripts.js

--- projects/2021/06/js/scripts.js
+++ projects/2021/06/js/scripts.js
@@ -68,19 +68,4 @@
   })(jQuery); // End of use strict
   
 
-   //PWA Service Worker
-import swURL from 'https://alb0174.github.io/main/projects/2021/06/service-worker.js';
-
-// Register the service worker
-if ('serviceWorker' in navigator) {
-  // Wait for the 'load' event to not block other work
-  window.addEventListener('load', async () => {
-    // Try to register the service worker.
-    try {
-      const reg = await navigator.serviceWorker.register(swURL);
-      console.log('Service worker registered! 😎', reg);
-    } catch (err) {
-      console.log('😥 Service worker registration failed: ', err);
-    }
-  });
-}
+  
