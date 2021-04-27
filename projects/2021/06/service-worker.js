22e01fd844c6dee1a07e839468d8b7a3bb4ec751
alb0174 <41185492+alb0174@users.noreply.github.com>
2021-04-28 01:16:59 +0200

    pure css

--- projects/2021/06/service-worker.js
+++ projects/2021/06/service-worker.js
@@ -3,7 +3,7 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
 // List the files to precache
-const precacheResources = ['/main/projects/2021/06/', '/main/projects/2021/06/index.html', '/main/projects/2021/06/css/styles.css', '/main/projects/2021/06/js/scripts.js'];
+const precacheResources = ['/main/projects/2021/06/', '/main/projects/2021/06/index.html', '/main/projects/2021/06/css/styles.pure.css', '/main/projects/2021/06/js/scripts.js'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
